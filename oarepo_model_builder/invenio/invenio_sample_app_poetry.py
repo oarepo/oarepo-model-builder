9602b5a16cb1142328ff9c9081414424ae1c8f60
Mirek Simek <miroslav.simek@vscht.cz>
2021-12-21 20:32:14 +0100

    Poetry builder with runtime and dev dependencies

--- oarepo_model_builder/invenio/invenio_sample_app_poetry.py
+++ oarepo_model_builder/invenio/invenio_sample_app_poetry.py
@@ -14,19 +14,6 @@ class InvenioSampleAppPoetryBuilder(OutputBuilder):
             'pyproject.toml'
         )
 
-        output.setdefault("tool.poetry",
-                          "name", self.settings.package_base.replace('_', '-'),
-                          "version", "0.0.1",
-                          "description", f"A sample application for {self.settings.package}",
-                          "authors", [])
-
-        output.setdefault("build-system",
-                          "requires", ['poetry-core>=1.0.0'],
-                          "build-backend", "poetry.core.masonry.api")
-
-        output.setdefault("tool.poetry.dependencies", "python", "^3.9")
-        output.setdefault("tool.poetry.dependencies", "python", "^3.9")
-
         output.setdefault("tool.poetry.dependencies.invenio",
                           'version', '^3.5.0a1',
                           'extras', ["base", "auth", "metadata", "files", "postgresql", "elasticsearch7"],
