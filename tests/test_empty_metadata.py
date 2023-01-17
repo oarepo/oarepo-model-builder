dcfd15986ab07aed13aac757f75857d09dbfabdf
Mirek Simek <miroslav.simek@gmail.com>
2023-01-17 16:09:53 +0100

    Empty element (None) works

---
+++ tests/test_empty_metadata.py
@@ -0,0 +1,37 @@
+import json
+import os
+import re
+import shutil
+import sys
+from pathlib import Path
+from tempfile import mkdtemp, tempdir
+
+from oarepo_model_builder.entrypoints import create_builder_from_entrypoints, load_model
+from oarepo_model_builder.fs import FileSystem
+from tests.mock_filesystem import MockFilesystem
+from tests.utils import assert_python_equals
+
+OAREPO_USE = "oarepo:use"
+
+
+def test_empty_metadata():
+    schema = load_model(
+        "test.yaml",
+        "test",
+        model_content={
+            "version": "1.0.0",
+            OAREPO_USE: "invenio",
+            "model": {"properties": None},
+        },
+        isort=False,
+        black=False,
+    )
+
+    filesystem = FileSystem()
+    builder = create_builder_from_entrypoints(filesystem=filesystem, overwrite=True)
+
+    tmpdir = mkdtemp()
+    try:
+        builder.build(schema, tmpdir)
+    finally:
+        shutil.rmtree(tmpdir)
