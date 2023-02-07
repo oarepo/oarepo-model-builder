57b0ac5651446a7a4319185e2e744ec5924cc561
Alzpetka <lili3.pokorna@seznam.cz>
2023-02-07 09:13:37 +0100

    facets tests full

--- tests/test_facets.py
+++ tests/test_facets.py
@@ -1,9 +1,5 @@
-import json
+
 import os
-import re
-from io import StringIO
-from pathlib import Path
-from typing import Dict
 
 from oarepo_model_builder.entrypoints import create_builder_from_entrypoints, load_model
 from oarepo_model_builder.fs import InMemoryFileSystem
@@ -240,7 +236,7 @@ def test_nest_obj():
                 "use": "invenio",
                 "properties": {
                     "b_nes": {
-                        "type": "object",
+                        "type": "nested",
                         "properties": {
                             "c": {
                                 "type": "keyword",
@@ -296,28 +292,171 @@ from oarepo_runtime.facets.nested_facet import NestedLabeledFacet
 
 
 
-b-nes_c = NestedLabeledFacet(path =" b", nested_facet=TermsFacet(field = "b-nes.c"))
+b_nes_c = NestedLabeledFacet(path =" b", nested_facet=TermsFacet(field = "b_nes.c"))
 
 
 
-b-nes_d_keyword = NestedLabeledFacet(path =" b", nested_facet=TermsFacet(field = "b-nes.d.keyword"))
+b_nes_d_keyword = NestedLabeledFacet(path =" b", nested_facet=TermsFacet(field = "b_nes.d.keyword"))
 
 
 
-b-nes_f_g = NestedLabeledFacet(path =" b", nested_facet=TermsFacet(field = "b-nes.f.g"))
+b_nes_f_g = NestedLabeledFacet(path =" b", nested_facet=TermsFacet(field = "b_nes.f.g"))
 
 
 
-b-obj_c = TermsFacet(field = "b-obj.c")
+b_obj_c = TermsFacet(field = "b_obj.c")
 
 
 
-b-obj_d_keyword = TermsFacet(field = "b-obj.d.keyword")
+b_obj_d_keyword = TermsFacet(field = "b_obj.d.keyword")
 
 
-#???????????????????????????????????????????#
-b-obj_f_g = TermsFacet(field = "b-obj.f.g")
 
+b_obj_f_g = NestedLabeledFacet(path ="b_obj.f", nested_facet=TermsFacet(field = "b_obj.f.g"))
+
+
+
+_id = TermsFacet(field = "id")
+
+
+
+created = TermsFacet(field = "created")
+
+
+
+updated = TermsFacet(field = "updated")
+
+
+
+_schema = TermsFacet(field = "$schema")
+""",
+    )
+
+def test_array():
+    schema = load_model(
+        DUMMY_YAML,
+        "test",
+        model_content={
+            "model": {
+                "use": "invenio",
+                "properties": {
+
+                    "a[]": "keyword",
+                    "b[]": "fulltext",
+                    "c[]": "fulltext+keyword"
+                },
+            },
+        },
+        isort=False,
+        black=False,
+    )
+
+    filesystem = InMemoryFileSystem()
+    builder = create_builder_from_entrypoints(filesystem=filesystem)
+
+    builder.build(schema, "")
+
+    data = builder.filesystem.open(
+        os.path.join("test", "services", "records", "facets.py")
+    ).read()
+    print(data)
+    assert re.sub(r"\s", "", data) == re.sub(
+        r"\s",
+        "",
+        """
+        \"""Facet definitions.\"""
+
+from invenio_records_resources.services.records.facets import TermsFacet
+from invenio_search.engine import dsl
+from oarepo_runtime.facets.nested_facet import NestedLabeledFacet
+
+
+
+
+a = TermsFacet(field = "a")
+
+
+
+c_keyword = TermsFacet(field = "c.keyword")
+
+
+_id = TermsFacet(field = "id")
+
+
+
+created = TermsFacet(field = "created")
+
+
+
+updated = TermsFacet(field = "updated")
+
+
+
+_schema = TermsFacet(field = "$schema")
+""",
+    )
+
+def test_array_nested():
+    schema = load_model(
+        DUMMY_YAML,
+        "test",
+        model_content={
+            "model": {
+                "use": "invenio",
+                "properties": {
+                    "obj": {
+                        "type": "object",
+                        "properties": {
+                            "arr": {
+                                "type": "array",
+                                "items": {
+                                    "type": "nested",
+                                    "properties": {
+                                        "d": {"type": "keyword"},
+                                        "e": {
+                                            "type": "object",
+                                            "properties": {
+                                                "f": "keyword"
+                                            }
+                                        }
+
+                                    }
+                                }
+
+                            }
+                        }
+
+                    },
+                },
+            },
+        },
+        isort=False,
+        black=False,
+    )
+
+    filesystem = InMemoryFileSystem()
+    builder = create_builder_from_entrypoints(filesystem=filesystem)
+
+    builder.build(schema, "")
+
+    data = builder.filesystem.open(
+        os.path.join("test", "services", "records", "facets.py")
+    ).read()
+    print(data)
+    assert re.sub(r"\s", "", data) == re.sub(
+        r"\s",
+        "",
+        """
+        \"""Facet definitions.\"""
+
+from invenio_records_resources.services.records.facets import TermsFacet
+from invenio_search.engine import dsl
+from oarepo_runtime.facets.nested_facet import NestedLabeledFacet
+
+
+obj_arr_d = NestedLabeledFacet(path ="obj.arr", nested_facet=TermsFacet(field = "obj.arr.d"))
+
+obj_arr_e_f = NestedLabeledFacet(path ="obj.arr", nested_facet=TermsFacet(field = "obj.arr.e.f"))
 
 
 _id = TermsFacet(field = "id")
