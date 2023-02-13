cf665c29bf8784fe2367dcf75e12e4b50a06cdd8
Mirek Simek <miroslav.simek@gmail.com>
2023-02-13 13:05:12 +0100

    bad chars fix

--- oarepo_model_builder/invenio/invenio_record_search.py
+++ oarepo_model_builder/invenio/invenio_record_search.py
@@ -173,8 +173,9 @@ class InvenioRecordSearchOptionsBuilder(InvenioBaseClassPythonBuilder):
                 self.search_options_data.append(
                     {convert_name_to_python(name): search_options}
                 )
-            facets_name = "facets." + name
-            self.facets_definition.append({convert_name_to_python(name): facets_name})
+            self.facets_definition.append(
+                {name: "facets." + convert_name_to_python(name)}
+            )
 
     def process_search_options(self, data, field_class):
         text = ""
