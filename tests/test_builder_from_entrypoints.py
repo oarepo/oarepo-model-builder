f4df97d9c726921814de8f6ca431b23409f02af2
Mirek Simek <miroslav.simek@gmail.com>
2023-02-24 12:23:28 +0100

    fixed tests

--- tests/test_builder_from_entrypoints.py
+++ tests/test_builder_from_entrypoints.py
@@ -38,7 +38,7 @@ def test_include_invenio():
         """
 from invenio_records_resources.services.records.schema import BaseRecordSchema as InvenioBaseRecordSchema
 from marshmallow import ValidationError
-from marshmallow import validates as ma_validates
+from marshmallow import validate as ma_validate
 import marshmallow as ma
 from marshmallow import fields as ma_fields
 from marshmallow_utils import fields as mu_fields
--- tests/test_schema_props.py
+++ tests/test_schema_props.py
@@ -35,7 +35,7 @@ def test_enum():
         """
 from invenio_records_resources.services.records.schema import BaseRecordSchema as InvenioBaseRecordSchema
 from marshmallow import ValidationError
-from marshmallow import validates as ma_validates
+from marshmallow import validate as ma_validate
 import marshmallow as ma
 from marshmallow import fields as ma_fields
 from marshmallow_utils import fields as mu_fields
