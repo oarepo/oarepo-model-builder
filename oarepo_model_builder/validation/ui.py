25be187e78b2520dc460ac241bb51fd2b3aaaaee
Mirek Simek <miroslav.simek@gmail.com>
2023-03-08 12:51:08 +0100

    proper ui validation

---
+++ oarepo_model_builder/validation/ui.py
@@ -0,0 +1,26 @@
+import marshmallow as ma
+from marshmallow import fields
+from .model_validation import model_validator
+
+
+class UIPropertyValidator(ma.Schema):
+    marshmallow = fields.Nested(
+        lambda: model_validator.validator_class("property-marshmallow")()
+    )
+
+    class Meta:
+        unknown = ma.INCLUDE
+
+
+class ObjectUIPropertyValidator(ma.Schema):
+    marshmallow = fields.Nested(
+        lambda: model_validator.validator_class("property-marshmallow-object")()
+    )
+
+    class Meta:
+        unknown = ma.INCLUDE
+
+
+class ObjectPropertyUISchema(ma.Schema):
+    # x/[type object]
+    ui = fields.Nested(lambda: model_validator.validator_class("object-property-ui")())
