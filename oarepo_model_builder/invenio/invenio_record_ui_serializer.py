7c9d42250a91edccdbd0147265dd7ba9e3656548
Mirek Simek <miroslav.simek@gmail.com>
2023-03-10 10:29:41 +0100

    predefined model schema class - referencing from config & ui serializer (#129)

--- oarepo_model_builder/invenio/invenio_record_ui_serializer.py
+++ oarepo_model_builder/invenio/invenio_record_ui_serializer.py
@@ -5,3 +5,15 @@ class InvenioRecordUISerializerBuilder(InvenioBaseClassPythonBuilder):
     TYPE = "invenio_record_ui_serializer"
     class_config = "record-ui-serializer-class"
     template = "record-ui-serializer"
+
+    def finish(self, **extra_kwargs):
+
+        ui_record_schema_class = (
+            self.current_model.get("ui", {})
+            .get("marshmallow", {})
+            .get("schema-class", self.current_model.record_schema_class)
+        )
+
+        return super().finish(
+            ui_record_schema_class=ui_record_schema_class, **extra_kwargs
+        )
