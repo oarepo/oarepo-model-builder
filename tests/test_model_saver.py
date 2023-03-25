11c24b83a510ecba57cfebae0ff614e2e17e619c
Mirek Simek <miroslav.simek@gmail.com>
2023-03-25 08:32:45 +0100

    ui base class

--- tests/test_model_saver.py
+++ tests/test_model_saver.py
@@ -440,9 +440,18 @@ def test_model_saver_invenio():
         "package-path": "test",
         "ui": {
             "marshmallow": {
+<<<<<<< Updated upstream
                 "generate": True,
                 "schema-class": "test.services.records.ui_schema.TestUISchema",
                 "base-classes": ["ma.Schema"],
+=======
+                "base-classes": ["InvenioUISchema"],
+                "imports": [
+                    {"import": "oarepo_runtime.ui.marshmallow.InvenioUISchema"}
+                ],
+                "schema-class": "test.services.records.ui_schema.TestUISchema",
+                "generate": True,
+>>>>>>> Stashed changes
             }
         },
         "invenio-record-extra-code": "",
