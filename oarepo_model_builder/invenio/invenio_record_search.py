47546ce961121027392591780b0cdd5fe39df310
Mirek Simek <miroslav.simek@gmail.com>
2023-04-18 10:35:29 +0200

    Facets refactor into a function (to be called from ui)

--- oarepo_model_builder/invenio/invenio_record_search.py
+++ oarepo_model_builder/invenio/invenio_record_search.py
@@ -5,6 +5,7 @@ from ..datatypes import datatypes
 from ..outputs.json_stack import JSONStack
 from ..utils.deepmerge import deepmerge
 from .invenio_base import InvenioBaseClassPythonBuilder
+from typing import Set
 
 OAREPO_FACETS_PROPERTY = "facets"
 OAREPO_SORTABLE_PROPERTY = "sortable"
@@ -20,12 +21,10 @@ class InvenioRecordSearchOptionsBuilder(InvenioBaseClassPythonBuilder):
         self.template = "record-search-options"
         self.search_options_data = []
         self.sort_options_data = []
-        self.search_options_stack = JSONStack()
         self.facets_definition = []
         self.settings = settings
         if "sortable" in schema.schema:
             self.process_top_sortable(schema.schema["sortable"])
-        self.facet_switch = self.schema.model.get("searchable", True)
         self.facet_stack = []
         self.imports = set()
 
@@ -58,13 +57,8 @@ class InvenioRecordSearchOptionsBuilder(InvenioBaseClassPythonBuilder):
     def enter_model_element(self):
         schema_element_type = self.stack.top.schema_element_type
         data = self.stack.top.data
-        recurse = True
 
-        if recurse:
-            self.build_children()
-
-        if not self.search_options_stack:
-            return
+        self.build_children()
 
         if schema_element_type == "property":
             sort_definition = data.get(OAREPO_SORTABLE_PROPERTY, None)
@@ -73,81 +67,19 @@ class InvenioRecordSearchOptionsBuilder(InvenioBaseClassPythonBuilder):
                 self.sort_options_data.append(
                     self.process_sort_options(self.stack.path, sort_definition)
                 )
-        if self.stack.top.schema_element_type in ["property", "items"]:
-            d_type = datatypes.get_datatype(
-                self.stack.top.data,
-                self.stack.top.key,
-                self.current_model,
-                self.schema,
-                self.stack,
-            )
-            for _imp in d_type.facet_imports:
-                import_path = _imp.get("import")
-                import_alias = _imp.get("alias")
-                if import_path:
-                    self.imports.add((import_path, import_alias))
 
-            if d_type.get_facet(None, ""):
-                stack_data = []
-                for s in self.stack.stack:
-                    type = s.schema_element_type
-                    if type and type == "property":
-                        stack_data.append(
-                            datatypes.get_datatype(
-                                s.data,
-                                s.key,
-                                self.current_model,
-                                self.schema,
-                                self.stack,
-                            )
-                        )
-                    elif type == "items":
-                        stack_data.append(
-                            datatypes.get_datatype(
-                                s.data,
-                                None,
-                                self.current_model,
-                                self.schema,
-                                self.stack,
-                            )
-                        )
-                skip = False
-
-                if self.facet_switch:
-                    for s in stack_data:
-                        if (
-                            "facets" in s.definition
-                            and "searchable" in s.definition["facets"]
-                            and not s.definition["facets"]["searchable"]
-                        ):
-                            skip = True
-                            break
-                else:
-                    skip = True
-                    for s in stack_data:
-                        if "facets" in s.definition:
-                            if (
-                                "searchable" in s.definition["facets"]
-                                and s.definition["facets"]["searchable"]
-                            ):
-                                skip = False
-                                break
-                            elif (
-                                "key" in s.definition["facets"]
-                                or "field" in s.definition["facets"]
-                            ):
-                                skip = False
-                                break
-
-                if not skip:
-                    # facet, path = datatypes.facet(stack_data)
-                    facet_obj = datatypes.facet(stack_data)
-                    for f in facet_obj:
-                        facet = f["facet"]
-                        path = f["path"]
-                        self.search_options_data.append({path: facet})
-                        search_ops_name = "facets." + path
-                        self.facets_definition.append({path: search_ops_name})
+        facet_obj = get_facet_details(
+            stack=self.stack,
+            current_model=self.current_model,
+            schema=self.schema,
+            imports=self.imports,
+        )
+        for f in facet_obj:
+            facet = f["facet"]
+            path = f["path"]
+            self.search_options_data.append({path: facet})
+            search_ops_name = "facets." + path
+            self.facets_definition.append({path: search_ops_name})
 
     def process_name(self, path, type):
         path_array = (path.split("/"))[2:]
@@ -180,3 +112,80 @@ class InvenioRecordSearchOptionsBuilder(InvenioBaseClassPythonBuilder):
             field = "-" + field
 
         return {key: dict(fields=[field])}
+
+
+def get_facet_details(stack, current_model, schema, imports: Set):
+    schema_element_type = stack.top.schema_element_type
+    data = stack.top.data
+
+    skip = True
+
+    if stack.top.schema_element_type in ["property", "items"]:
+        d_type = datatypes.get_datatype(
+            stack.top.data,
+            stack.top.key,
+            current_model,
+            schema,
+            stack,
+        )
+        for _imp in d_type.facet_imports:
+            import_path = _imp.get("import")
+            import_alias = _imp.get("alias")
+            if import_path:
+                imports.add((import_path, import_alias))
+
+        if d_type.get_facet(None, ""):
+            stack_data = []
+            for s in stack.stack:
+                type = s.schema_element_type
+                if type and type == "property":
+                    stack_data.append(
+                        datatypes.get_datatype(
+                            s.data,
+                            s.key,
+                            current_model,
+                            schema,
+                            stack,
+                        )
+                    )
+                elif type == "items":
+                    stack_data.append(
+                        datatypes.get_datatype(
+                            s.data,
+                            None,
+                            current_model,
+                            schema,
+                            stack,
+                        )
+                    )
+            skip = False
+
+            if current_model.get("searchable", True):
+                for s in stack_data:
+                    if (
+                        "facets" in s.definition
+                        and "searchable" in s.definition["facets"]
+                        and not s.definition["facets"]["searchable"]
+                    ):
+                        skip = True
+                        break
+            else:
+                skip = True
+                for s in stack_data:
+                    if "facets" in s.definition:
+                        if (
+                            "searchable" in s.definition["facets"]
+                            and s.definition["facets"]["searchable"]
+                        ):
+                            skip = False
+                            break
+                        elif (
+                            "key" in s.definition["facets"]
+                            or "field" in s.definition["facets"]
+                        ):
+                            skip = False
+                            break
+    if skip:
+        return []
+
+    return datatypes.facet(stack_data)
