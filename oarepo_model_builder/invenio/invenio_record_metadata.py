d15ae9066bdb959f7e0aa03d4a73f6cfeb8fa65f
Ronald Krist <ronald.krist@cesnet.cz>
2023-05-30 10:11:52 +0200

    model builder 4 adaptation, in progress

--- oarepo_model_builder/invenio/invenio_record_metadata.py
+++ oarepo_model_builder/invenio/invenio_record_metadata.py
@@ -5,3 +5,11 @@ class InvenioRecordMetadataBuilder(InvenioBaseClassPythonBuilder):
     TYPE = "invenio_record_metadata"
     section = "record-metadata"
     template = "record-metadata"
+    record_section = "section_record"
+    record_metadata_section = "section_record_metadata"
+
+
+    def finish(self, **extra_kwargs):
+        super().finish(record=getattr(self.current_model, self.record_section).config,
+                       record_metadata=getattr(self.current_model, self.record_metadata_section).config,
+                       **extra_kwargs)
