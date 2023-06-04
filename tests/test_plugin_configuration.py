1580f16350bf3cee11dc5973e86da9e9eb2da8eb
Mirek Simek <miroslav.simek@gmail.com>
2023-06-04 22:21:11 +0200

    Moved plugins to the root of model file as they are global, not record specific

--- tests/test_plugin_configuration.py
+++ tests/test_plugin_configuration.py
@@ -2,19 +2,18 @@ from oarepo_model_builder.builder import ModelBuilder
 from oarepo_model_builder.outputs.jsonschema import JSONSchemaOutput
 from oarepo_model_builder.outputs.mapping import MappingOutput
 from oarepo_model_builder.schema import ModelSchema
-from oarepo_model_builder.utils.dict import dict_get
 
 
 def test_output_disabled():
     builder = ModelBuilder()
     schema = ModelSchema(
-        "", {"record": {"plugins": {"output": {"disable": "__all__"}}}}
+        "", {"record": {}, "plugins": {"output": {"disable": "__all__"}}}
     )
     builder.set_schema(schema)
     assert (
         builder._filter_classes(
             [JSONSchemaOutput, MappingOutput],
-            dict_get(schema.schema, ["record"]),
+            schema.schema,
             "output",
         )
         == []
@@ -24,14 +23,14 @@ def test_output_disabled():
 def test_output_disabled_single():
     builder = ModelBuilder()
     schema = ModelSchema(
-        "", {"record": {"plugins": {"output": {"disable": ["jsonschema"]}}}}
+        "", {"record": {}, "plugins": {"output": {"disable": ["jsonschema"]}}}
     )
     builder.set_schema(schema)
     assert set(
         x.TYPE
         for x in builder._filter_classes(
             [JSONSchemaOutput, MappingOutput],
-            dict_get(schema.schema, ["record"]),
+            schema.schema,
             "output",
         )
     ) == {"mapping"}
@@ -42,9 +41,8 @@ def test_output_enabled():
     schema = ModelSchema(
         "",
         {
-            "record": {
-                "plugins": {"output": {"disable": "__all__", "enable": ["mapping"]}}
-            }
+            "record": {},
+            "plugins": {"output": {"disable": "__all__", "enable": ["mapping"]}},
         },
     )
     builder.set_schema(schema)
@@ -52,7 +50,7 @@ def test_output_enabled():
         x.TYPE
         for x in builder._filter_classes(
             [JSONSchemaOutput, MappingOutput],
-            dict_get(schema.schema, ["record"]),
+            schema.schema,
             "output",
         )
     ) == {"mapping"}
@@ -63,21 +61,15 @@ def test_output_enabled_import():
     schema = ModelSchema(
         "",
         {
-            "record": {
-                "plugins": {
-                    "output": {
-                        "include": [
-                            "oarepo_model_builder.outputs.mapping:MappingOutput"
-                        ]
-                    }
+            "record": {},
+            "plugins": {
+                "output": {
+                    "include": ["oarepo_model_builder.outputs.mapping:MappingOutput"]
                 }
-            }
+            },
         },
     )
     builder.set_schema(schema)
     assert set(
-        x.TYPE
-        for x in builder._filter_classes(
-            [], dict_get(schema.schema, ["record"]), "output"
-        )
+        x.TYPE for x in builder._filter_classes([], schema.schema, "output")
     ) == {"mapping"}
