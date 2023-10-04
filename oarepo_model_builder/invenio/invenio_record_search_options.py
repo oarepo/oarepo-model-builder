7910c5eccdd14d79cd02b1396da2794b4f54d80f
Alzpetka <lili3.pokorna@seznam.cz>
2023-10-04 13:09:55 +0200

    default facet group

--- oarepo_model_builder/invenio/invenio_record_search_options.py
+++ oarepo_model_builder/invenio/invenio_record_search_options.py
@@ -13,17 +13,22 @@ class InvenioRecordSearchOptionsBuilder(InvenioBaseClassPythonBuilder):
     def finish(self, **extra_kwargs):
         facets: List[FacetDefinition] = get_distinct_facets(self.current_model)
         facet_groups = {}
+        default_group = []
         search_data = []
         for f in facets:
             for group in f.facet_groups:
-                if group not in facet_groups.keys():
-                    facet_groups[group] = {}
-                facet_groups[group][f.path] = "facets." + f.path
+                if group != 'default':
+                    if group not in facet_groups.keys():
+                        facet_groups[group] = {}
+                    facet_groups[group][f.path] = "facets." + f.path
+                if group == 'default':
+                    default_group.append({f.path: "facets." + f.path})
             search_data.append({f.path: "facets." + f.path})
         if "sortable" in self.current_model.definition:
             sort_options = self.current_model.definition["sortable"]
         else:
             sort_options = {}
         extra_kwargs["facet_groups"] = facet_groups
+        extra_kwargs["default_group"] = default_group
         extra_kwargs["sort_definition"] = sort_options
         super().finish(**extra_kwargs)
