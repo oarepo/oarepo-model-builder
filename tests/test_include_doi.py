26442f2b5cdd2fdd12df2b67070c41975650d75d
Alzpetka <lili3.pokorna@seznam.cz>
2024-09-27 14:46:14 +0200

    doi model

---
+++ tests/test_include_doi.py
@@ -0,0 +1,39 @@
+import os
+import re
+
+from oarepo_model_builder.entrypoints import create_builder_from_entrypoints, load_model
+from oarepo_model_builder.fs import InMemoryFileSystem
+
+from .utils import strip_whitespaces
+
+OAREPO_USE = "use"
+
+
+
+def test_include_invenio():
+    schema = load_model(
+        "test.yaml",  # NOSONAR
+        model_content={
+            "version": "1.0.0",
+
+            "record": {
+                "module": {"qualified": "test"},
+                OAREPO_USE: ["invenio", "doi"],
+                "properties": {"a": {"type": "keyword", "required": True}},
+            },
+        },
+        isort=False,
+        black=False,
+        autoflake=False,
+    )
+
+    filesystem = InMemoryFileSystem()
+    builder = create_builder_from_entrypoints(filesystem=filesystem)
+
+    builder.build(schema, "record", ["record"], "")
+
+    data = builder.filesystem.open(
+        os.path.join("test", "services", "records", "config.py")
+    ).read()
+    data = str(data)
+    assert "components=[*PermissionsPresetsConfigMixin.components,*InvenioRecordServiceConfig.components,DoiComponent]" in re.sub(r"\s", "", data)
