f0bba5e546fb61303df25664be052970ab2fc434
Alzbeta Pokorna <alzbetapokorna@eduroam-tlh43.cesnet.cz>
2025-01-08 14:28:03 +0100

    verbose

--- run-tests.sh
+++ run-tests.sh
@@ -158,7 +158,7 @@ if [ "$1" == "--server" ] ; then
 fi
 
 cat complex-model/data/sample_data.yaml
-$TEST_VENV/bin/pytest tests-model -vvv
+$TEST_VENV/bin/pytest tests-model -vv -s
 
 
 echo "All tests succeeded"
